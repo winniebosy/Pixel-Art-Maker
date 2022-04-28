ef965bbf78dd0890b0832fd1e2e176d718333f52
Winnie Bosibori <winniebosy@gmail.com>
2022-04-28 17:39:39 +0300

    variables for all the elements to be used

--- designs.js
+++ designs.js
@@ -1,10 +1,23 @@
+"use strict";;
+// select form
+const sizePicker = document.querySelector('#sizePicker');
 // Select color input
+let colorPicker = document.querySelector('#colorPicker');
 // Select size input
+const inputHeight = document.querySelector('#inputHeight');
+const inputWidth = document.querySelector('#inputWidth');
+// table element
+const pixelCanvas = document.querySelector('#pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
 
+
+
+
+
+
 function makeGrid() {
 
-// Your code goes here!
+    // Your code goes here!
 
 }
