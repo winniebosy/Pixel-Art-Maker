bda494d7daaf949cf31fff3f07e017df1b605b1a
Winnie Bosibori <winniebosy@gmail.com>
2022-04-29 18:27:14 +0300

    html files

--- designs.js
+++ designs.js
@@ -2,11 +2,8 @@
 // select form
 const sizePicker = document.querySelector('#sizePicker');
 // Select color input
-let colorPicker = document.querySelector('#colorPicker');
+const colorPicker = document.querySelector('#colorPicker');
 // Select size input
-const inputHeight = document.querySelector('#inputHeight');
-const inputWidth = document.querySelector('#inputWidth');
-// table element
 const pixelCanvas = document.querySelector('#pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
@@ -15,9 +12,15 @@ const pixelCanvas = document.querySelector('#pixelCanvas');
 
 
 
-
 function makeGrid() {
+    // getting the input value for
 
-    // Your code goes here!
+    let inputHeight = document.querySelector('#inputHeight').value;
+    let inputWidth = document.querySelector('#inputWidth').value;
+    // creating td and tr
+    for (let i = 0; i <= inputHeight; i++) {
+        let gridRow = document.createElement('tr');
+        pixelCanvas.appendChild('gridRow')
+    }
 
-}
+};
